f0a114e950017f3f25f044ea064263b772f43a3f
Tim Newman <timnewman9962@gmail.com>
2020-09-14 17:31:36 -0400

    Week 4 challenges redone with lambda and linq

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ChallengesWithTestsMark8
 {
@@ -9,30 +11,38 @@ namespace ChallengesWithTestsMark8
         {
             if (numbers == null)
                 return 0;
-            int num = 0;
-            foreach(var n in numbers)
-            {
-                num += n % 2 == 0 ? n : -n;
-            }
-            return num;
+            //int num = 0;
+            //foreach(var n in numbers)
+            //{
+            //    num += n % 2 == 0 ? n : -n;
+            //}
+            //return num;
+
+            return numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(x => x % 2 != 0).Sum();
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            int shortest = str1.Length;
-            shortest = str2.Length < shortest ? str2.Length : shortest;
-            shortest = str3.Length < shortest ? str3.Length : shortest;
-            shortest = str4.Length < shortest ? str4.Length : shortest;
-            return shortest;
+            //int shortest = str1.Length;
+            //shortest = str2.Length < shortest ? str2.Length : shortest;
+            //shortest = str3.Length < shortest ? str3.Length : shortest;
+            //shortest = str4.Length < shortest ? str4.Length : shortest;
+            //return shortest;
+
+            var arr = new string[] { str1, str2, str3, str4 };
+            return arr.Min(s => s.Length);
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            int smallest = number1;
-            smallest = Math.Min(smallest, number2);
-            smallest = Math.Min(smallest, number3);
-            smallest = Math.Min(smallest, number4);
-            return smallest;
+            //int smallest = number1;
+            //smallest = Math.Min(smallest, number2);
+            //smallest = Math.Min(smallest, number3);
+            //smallest = Math.Min(smallest, number4);
+            //return smallest;
+
+            var arr = new int[] { number1, number2, number3, number4 };
+            return arr.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
@@ -58,30 +68,34 @@ namespace ChallengesWithTestsMark8
         {
             if (objs == null)
                 return true;
-            int nulls = 0;
-            foreach (var item in objs)
-            {
-                if (item == null)
-                    nulls++;
-            }
-            return nulls > objs.Length - nulls;
+            //int nulls = 0;
+            //foreach (var item in objs)
+            //{
+            //    if (item == null)
+            //        nulls++;
+            //}
+            //return nulls > objs.Length - nulls;
+
+            return objs.Count(x => x == null) > objs.Count() / 2;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            if (numbers == null)
+            if (numbers == null || numbers.Length == 0)
                 return 0;
-            int sum = 0;
-            int nCnt = 0;
-            foreach (var item in numbers)
-            {
-                if (item % 2 == 0)
-                {
-                    sum += item;
-                    nCnt++;
-                }
-            }
-            return nCnt == 0 ? 0 : (double)sum / nCnt;
+            //int sum = 0;
+            //int nCnt = 0;
+            //foreach (var item in numbers)
+            //{
+            //    if (item % 2 == 0)
+            //    {
+            //        sum += item;
+            //        nCnt++;
+            //    }
+            //}
+            //return nCnt == 0 ? 0 : (double)sum / nCnt;
+
+            return numbers.Any(x => x % 2 == 0) ? (double)numbers.Where(x => x % 2 == 0).Average(): 0.0;
         }
 
         public int Factorial(int number)
